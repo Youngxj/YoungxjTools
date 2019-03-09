0ad13e24fc2c2e4b915474e496d7ff01619dfcc6
Youngxj <blog@youngxj.cn>
2019-03-09 20:54:12 +0800

    Update function.base.php
    修复关于编码的重大错误，取消全局xss防护，请足以防范，谢谢

--- function.base.php
+++ function.base.php
@@ -188,42 +188,6 @@ function utf8_urldecode($str)
   return html_entity_decode($str,null,'UTF-8');;
 }
 ?>
-<?php
-$_GET     && SafeFilter($_GET);
-$_POST    && SafeFilter($_POST);
-$_COOKIE  && SafeFilter($_COOKIE);
-//php防注入和XSS攻击通用过滤. 
-//by qq:831937
-function SafeFilter (&$arr) 
-{
-
- $ra=Array('/([\x00-\x08,\x0b-\x0c,\x0e-\x19])/','/script/','/javascript/','/vbscript/','/expression/','/applet/','/meta/','/xml/','/blink/','/link/','/style/','/embed/','/object/','/frame/','/layer/','/title/','/bgsound/','/base/','/onload/','/onunload/','/onchange/','/onsubmit/','/onreset/','/onselect/','/onblur/','/onfocus/','/onabort/','/onkeydown/','/onkeypress/','/onkeyup/','/onclick/','/ondblclick/','/onmousedown/','/onmousemove/','/onmouseout/','/onmouseover/','/onmouseup/','/onunload/');
-
- if (is_array($arr))
- {
-   foreach ($arr as $key => $value) 
-   {
-    if (!is_array($value))
-    {
-           //不对magic_quotes_gpc转义过的字符使用addslashes(),避免双重转义。
-      if (!get_magic_quotes_gpc())            
-      {
-            //给单引号（'）、双引号（"）、反斜线（\）与 NUL（NULL 字符）加上反斜线转义
-       $value  = addslashes($value);           
-     }
-           //删除非打印字符，粗暴式过滤xss可疑字符串
-     $value       = preg_replace($ra,'',$value);  
-          //去除 HTML 和 PHP 标记并转换为 HTML 实体   
-     $arr[$key]     = htmlentities(strip_tags($value)); 
-   }
-   else
-   {
-    SafeFilter($arr[$key]);
-  }
-}
-}
-}
-?>
 <?php 
 /**
  * 判断是否为邮箱
